56808908327685cc70cfffa1237780cbb460063f
Noha-Thabet <nohaathabet@gmail.com>
2024-03-05 19:05:02 +0100

    edit routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,27 @@ import Maps from "./Components/Maps/Maps";
 import Projects from "./Components/Projects/Projects";
 import Tasks from "./Components/Tasks/Tasks";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-// import Header from "./Components/Header/Header";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/* <Header /> */}
-    </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="team" element={<Team />} />
+        <Route path="calendars" element={<Calendars />} />
+        <Route path="Forms" element={<Forms />} />
+        <Route path="Blogs" element={<Blogs />} />
+        <Route path="Charts" element={<Charts />} />
+        <Route path="Contacts" element={<Contacts />} />
+        <Route path="Email" element={<Email />} />
+        <Route path="Invoices" element={<Invoices />} />
+        <Route path="Jobs" element={<Jobs />} />
+        <Route path="Maps" element={<Maps />} />
+        <Route path="Projects" element={<Projects />} />
+        <Route path="Tasks" element={<Tasks />} />
+      </Routes>
+    </>
   );
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Dashboard from "../Dashboard/Dashboard";
+import Navbar from "../Navbar/Navbar";
 import Social from "../Social/Social";
 import Transaction from "../Transaction/Transaction";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
+
       <Dashboard />
       <Social />
       <Transaction />
